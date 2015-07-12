90292611424e21626afb89acd9419dbee4b5be7e
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-12 20:05:47 +0200

    Storing temporary UI data in component state

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -5,11 +5,24 @@ App = React.createClass({
   // This mixin makes the getMeteorData method work
   mixins: [ReactMeteorData],
 
+  getInitialState() {
+    return {
+      hideCompleted: false
+    }
+  },
   // Loads itesm from the Tasks collection and puts them on this.data.tasks
   getMeteorData() {
-    return {
-      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+    let query = {};
+
+    if (this.state.hideCompleted) {
+      // If hide completed is checked, filter tasks
+      query = {checked: {$ne: true}};
     }
+
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+      incompleteCount: Tasks.find({checked: {$ne: true}}).count()
+    };
   },
   handleSubmit(event) {
     event.preventDefault();
@@ -31,11 +44,25 @@ App = React.createClass({
       return <Task key={task._id} task={task} />;
     });
   },
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: ! this.state.hideCompleted
+    });
+  },
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.data.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit}>
             <input
